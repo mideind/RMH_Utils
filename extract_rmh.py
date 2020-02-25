5433aa12a1f0f19e85c39ed6174190662fba0c61
haukurb <haukzi@gmail.com>
2020-02-25 18:46:18 +0000

    Added and renewed licenses

--- extract_rmh.py
+++ extract_rmh.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 """
-Reynir: Natural language processing for Icelandic
+    Reynir: Natural language processing for Icelandic
 
- RMH Converter
+     RMH Converter
 
-Copyright (C) 2019 Miðeind ehf.
+    Copyright (C) 2020 Miðeind ehf.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+       This program is free software: you can redistribute it and/or modify
+       it under the terms of the GNU General Public License as published by
+       the Free Software Foundation, either version 3 of the License, or
+       (at your option) any later version.
+       This program is distributed in the hope that it will be useful,
+       but WITHOUT ANY WARRANTY; without even the implied warranty of
+       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+       GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see http://www.gnu.org/licenses/.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
 
- Extraction and conversion utility for the RMH corpus
-
+     Extraction and conversion utility for the RMH corpus
 """
+
 
 import zipfile
 from pathlib import Path
